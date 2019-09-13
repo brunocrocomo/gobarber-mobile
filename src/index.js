25aa7b1b62af7b57b0653e45836f308955e7bac1
Bruno Martins Crocomo <bruno.crocomo@gmail.com>
2019-09-13 17:24:58 -0300

    Add routes and schedule list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
 import './config/ReactotonConfig';
 
-import Routes from './routes';
+import { store, persistor } from './store';
+import App from './App';
 
-export default function App() {
+// Ignore annoying warning from AnimatedComponent (react-native-reanimated)
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['AnimatedComponent']);
+
+export default function Index() {
     return (
-        <>
-            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-            <Routes />
-        </>
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+                <App />
+            </PersistGate>
+        </Provider>
     );
 }
